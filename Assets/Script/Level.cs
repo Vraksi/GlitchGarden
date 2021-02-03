98cf0456fef2df2e3a21429b82e033ae58761b5e
Vraksi <Jakobmetalv@gmail.com>
2021-02-03 19:30:37 +0100

    Animation start

---
+++ Assets/Script/Level.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] float timeBeforeLoad;
+
+    public void LoadSplashScreen()
+    {
+        SceneManager.LoadScene("SplashScreen");
+        LoadStartScene();
+    }
+
+    public void LoadStartScene()
+    {
+        StartCoroutine(StartSceneWithLoad());
+    }
+
+    public IEnumerator StartSceneWithLoad()
+    {
+        yield return new WaitForSeconds(timeBeforeLoad);
+        SceneManager.LoadScene(0);
+    }
+    
+}
