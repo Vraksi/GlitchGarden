cb8a721a53f7be0bbd808d7046aca79d1b5ed344
Vraksi <Jakobmetalv@gmail.com>
2021-02-09 16:57:43 +0100

    Animation

---
+++ Assets/Script/AttackerSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] bool spawn = true;
+    [SerializeField] float minTimeBetweenSpawns = 1f;
+    [SerializeField] float maxTimeBetweenSpawns = 5f;
+    [SerializeField] GameObject attackerPrefab;
+
+    // Start is called before the first frame update
+
+
+    IEnumerator Start()
+    {
+        while(spawn)
+        {
+            
+            yield return new WaitForSeconds(Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Shooter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectile;
+    [SerializeField] Transform gunPos;
+
+    public void Fire()
+    {
+        Instantiate(projectile, gunPos.position, transform.rotation);
+        return;
+    }
+}
