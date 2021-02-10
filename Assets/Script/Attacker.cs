56a6ce8fe3d06da41b10b73e6783c2cc4603d069
Vraksi <Jakobmetalv@gmail.com>
2021-02-10 21:39:02 +0100

    Onmousedown, instantiate Object

--- Assets/Script/Attacker.cs
+++ Assets/Script/Attacker.cs
@@ -6,7 +6,9 @@ public class Attacker : MonoBehaviour
 {
     [Range(0f, 5f)]
     float currentSpeed = 1f;
-
+    [SerializeField] float health = 100f;
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float deathVFXDuration = 1f;
 
     void Update()
     {
@@ -16,5 +18,32 @@ public class Attacker : MonoBehaviour
     public void SetMovementSpeed(float speed)
     {
         currentSpeed = speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Bullet bullet = collision.gameObject.GetComponent<Bullet>();
+        if (!bullet) { return; }
+        if (collision.tag == "Bullet")
+        {
+            DealDamage(bullet);            
+        }
+    }
+
+    private void DealDamage(Bullet bullet)
+    {
+        health -= bullet.getDamageProjectile();
+        bullet.Hit();
+        if ( health <= 0 )
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+        GameObject deathExplosion = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(deathExplosion, deathVFXDuration);
     }
 }
--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] float currentSpeed = 1f;
+    [SerializeField] float damageProjectile = 10f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.right * currentSpeed * Time.deltaTime);
+    }
+
+    public void Hit()
+    {
+        Destroy(gameObject);
+    }
+
+    public float getDamageProjectile()
+    {
+        return damageProjectile;
     }
 }
